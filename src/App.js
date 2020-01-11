b6adfe64e2653379d0b1032141fe8f8f39c265e2
ewerter <ewertontertuliano@gmail.com>
2020-01-10 20:19:28 -0600

    list feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {render} from "@testing-library/react";
 
 
 function App() {
+  const [itens, setItens] = useState([]);
+
+    const addItem = () => {
+      setItens([...itens, {
+        id: itens.length,
+        value: Math.floor(Math.random() * 10) + 1
+      }])
+    }
   return (
     <div className="App">
       <header className="App-header">
         <p>What time is it?</p>
+        <p>What number was added?</p>
+        <button onClick={addItem}>Add a number</button>
+        <ul>
+          {itens.map(iten =>(
+              <li key={iten.id}>{iten.value}</li>
+          ))}
+        </ul>
+
         
       </header>
-      
+      <button>Show hours</button>
     </div>
   );
 }
